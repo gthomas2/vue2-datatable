b2253bdf5abc6bc4f77c8bb316de15f64798343e
gthomas2 <brudinie@gmail.com>
2021-07-08 15:49:32 +0100

    Allow for a custom i18n string handler

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ Datatable.install = (Vue, options) => {
   const locale = options ? options.locale : {}
   
   // this might be the simplest i18n solution
-  Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
+  Vue.prototype.$i18nForDatatable = srcTxt => {
+    // GT Mod - allow for a custom i18n method.
+    if (Vue.$customI18nForDatatable) {
+      return Vue.$customI18nForDatatable(srcTxt)
+    }
+    return locale[srcTxt] || srcTxt
+  }
 
   Vue.component('Datatable', Datatable)
 }
